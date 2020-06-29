2bc48ca2172d3732eaf0a9dc22ccba4a2f9a458a
Julio Araujo <julio.araujo@adinet.com.uy>
2020-06-29 00:54:20 -0300

    Estructura

---
+++ Practico3/CapaLogicaTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("CapaLogicaTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard")>
+<Assembly: AssemblyProduct("CapaLogicaTest")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("c3bd04b2-ed4c-481b-a821-0bca6e1ff06d")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
